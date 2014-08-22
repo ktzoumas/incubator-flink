bc1616f3caa00a9b53177a33cf184e3d8da22011
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2014-08-22 18:17:48 +0200

    Perform TypeExtraction outside of Java Operators
    Before, FlatMapOperator, GroupReduceOperator, MapOperator, and MapPartitionOperator
    performed the Type extraction themselves while the other Operators had TypeInformation
    parameters. Now the are all unified, which makes it possible to use them from the
    Scala API.
    
    Also Key extraction for selector functions is moved outside of Keys.java

--- flink-java/src/main/java/org/apache/flink/api/java/operators/Keys.java
+++ flink-java/src/main/java/org/apache/flink/api/java/operators/Keys.java
@@ -126,14 +126,14 @@ public abstract class Keys<T> {
 
 		private final KeySelector<T, K> keyExtractor;
 		private final TypeInformation<K> keyType;
-		
-		public SelectorFunctionKeys(KeySelector<T, K> keyExtractor, TypeInformation<T> type) {
+
+		public SelectorFunctionKeys(KeySelector<T, K> keyExtractor, TypeInformation<T> inputType, TypeInformation<K> keyType) {
 			if (keyExtractor == null) {
 				throw new NullPointerException("Key extractor must not be null.");
 			}
-			
+
 			this.keyExtractor = keyExtractor;
-			this.keyType = TypeExtractor.getKeySelectorTypes(keyExtractor, type);
+			this.keyType = keyType;
 		}
 
 		public TypeInformation<K> getKeyType() {
