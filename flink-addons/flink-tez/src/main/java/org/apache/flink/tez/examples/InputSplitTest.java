e5b866fe312f6091211d1b2c4ea166455515deda
Kostas Tzoumas <kostas@Kostass-MacBook-Pro.local>
2014-10-08 10:51:32 +0200

    ...

--- flink-addons/flink-tez/src/main/java/org/apache/flink/tez/examples/InputSplitTest.java
+++ flink-addons/flink-tez/src/main/java/org/apache/flink/tez/examples/InputSplitTest.java
@@ -1,40 +1,55 @@
 package org.apache.flink.tez.examples;
 
 
+import org.apache.flink.api.common.Plan;
+import org.apache.flink.compiler.PactCompiler;
+import org.apache.flink.compiler.costs.DefaultCostEstimator;
+import org.apache.flink.compiler.plan.OptimizedPlan;
 import org.apache.flink.configuration.Configuration;
+import org.apache.flink.tez.dag.TezDAGGenerator;
 import org.apache.flink.tez.runtime.DataSourceProcessor;
 import org.apache.flink.tez.runtime.TezTaskConfig;
 import org.apache.flink.tez.runtime.input.FlinkInput;
 import org.apache.flink.tez.runtime.input.FlinkInputSplitGenerator;
 import org.apache.flink.tez.util.EncodingUtils;
 import org.apache.hadoop.security.Credentials;
+import org.apache.tez.client.TezClient;
 import org.apache.tez.common.TezUtils;
+import org.apache.tez.dag.api.DAG;
 import org.apache.tez.dag.api.DataSourceDescriptor;
 import org.apache.tez.dag.api.InputDescriptor;
 import org.apache.tez.dag.api.InputInitializerDescriptor;
 import org.apache.tez.dag.api.ProcessorDescriptor;
 import org.apache.tez.dag.api.TezConfiguration;
 import org.apache.tez.dag.api.Vertex;
+import org.apache.tez.dag.api.client.DAGClient;
+import org.apache.tez.dag.api.client.DAGStatus;
 import org.apache.tez.runtime.api.ProcessorContext;
+import org.apache.tez.runtime.library.api.TezRuntimeConfiguration;
 
 public class InputSplitTest {
 
     public static int DOP = 4;
 
+    static String jobName = "Input Split Tests";
 
     public static void main (String [] args) throws Exception {
 
         TezTaskConfig taskConfig = new TezTaskConfig(new Configuration());
 
+        TezConfiguration tezConf = new TezConfiguration();
 
-        TezConfiguration conf = new TezConfiguration();
 
-        conf.set("io.flink.processor.taskconfig", EncodingUtils.encodeObjectToString(taskConfig));
+        tezConf.setBoolean(TezConfiguration.TEZ_LOCAL_MODE, true);
+        tezConf.set("fs.defaultFS", "file:///");
+        tezConf.setBoolean(TezRuntimeConfiguration.TEZ_RUNTIME_OPTIMIZE_LOCAL_FETCH, true);
+
+        tezConf.set("io.flink.processor.taskconfig", EncodingUtils.encodeObjectToString(taskConfig));
 
         ProcessorDescriptor descriptor = ProcessorDescriptor.create(
                 DataSourceProcessor.class.getName());
 
-        descriptor.setUserPayload(TezUtils.createUserPayloadFromConf(conf));
+        descriptor.setUserPayload(TezUtils.createUserPayloadFromConf(tezConf));
 
         Vertex dataSource = Vertex.create("Data Source", descriptor, DOP);
 
@@ -48,6 +63,41 @@ public class InputSplitTest {
         );
         dataSource.addDataSource("Input", dataSourceDescriptor);
 
+        DAG dag = DAG.create(jobName);
+
+        dag.addVertex(dataSource);
+
+        try {
+            TezClient tezClient = TezClient.create(jobName, tezConf);
+
+            tezClient.start();
+
+            try {
+
+                tezClient.waitTillReady();
+                System.out.println("Submitting DAG to Tez Client");
+                DAGClient dagClient = tezClient.submitDAG(dag);
+                System.out.println("Submitted DAG to Tez Client");
+
+                // monitoring
+                DAGStatus dagStatus = dagClient.waitForCompletion();
+
+                if (dagStatus.getState() != DAGStatus.State.SUCCEEDED) {
+                    System.out.println(jobName + " failed with diagnostics: " + dagStatus.getDiagnostics());
+                    System.exit(1);
+                }
+                System.out.println(jobName + " finished successfully");
+                System.exit(0);
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                tezClient.stop();
+            }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
 }
--- flink-addons/flink-tez/src/main/java/org/apache/flink/tez/runtime/input/FLinkInputSplitGenerator.java
+++ flink-addons/flink-tez/src/main/java/org/apache/flink/tez/runtime/input/FLinkInputSplitGenerator.java
@@ -3,6 +3,7 @@ package org.apache.flink.tez.runtime.input;
 
 import org.apache.flink.api.common.io.InputFormat;
 import org.apache.flink.core.io.InputSplit;
+import org.apache.flink.util.InstantiationUtil;
 import org.apache.tez.dag.api.event.VertexStateUpdate;
 import org.apache.tez.runtime.api.Event;
 import org.apache.tez.runtime.api.InputInitializer;
@@ -10,6 +11,7 @@ import org.apache.tez.runtime.api.InputInitializerContext;
 import org.apache.tez.runtime.api.events.InputDataInformationEvent;
 import org.apache.tez.runtime.api.events.InputInitializerEvent;
 
+import java.nio.ByteBuffer;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -28,8 +30,12 @@ public class FlinkInputSplitGenerator extends InputInitializer {
 
         LinkedList<Event> events = new LinkedList<Event>();
         for (int i = 0; i < splits.length; i++) {
-            InputDataInformationEvent event = new InputDataInformationEvent.createWithSerializedPayload()
+            byte [] bytes = InstantiationUtil.serializeObject(splits[i]);
+            ByteBuffer buf = ByteBuffer.wrap(bytes);
+            InputDataInformationEvent event = new InputDataInformationEvent.createWithSerializedPayload(0, buf);
+            events.add(event);
         }
+        return events;
     }
 
     @Override
